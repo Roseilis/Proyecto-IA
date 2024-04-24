bcda860a950c806e972f6eff8df57d4364a05d95
roseilis <160541184+Roseilis@users.noreply.github.com>
2024-04-24 18:31:09 -0400

    Update Código.py

--- Código.py
+++ Código.py
@@ -1,11 +1,5 @@
 from pyswip import Prolog
 
-
-
-from pyswip import Prolog, Functor, Variable
-
-
-
 prolog = Prolog()
 
 prolog.consult("progr.pl")
@@ -14,35 +8,18 @@ prolog.consult("progr.pl")
 
 def consultar_aprobacion(nombre, puntos, asistencia):
 
-  
+    consulta = f"progr('{nombre}', {puntos}, {asistencia}."
+   result = bool (list(prolog.query(consulta))):
 
-    resultado = Variable()
+     
 
-  
+    
 
-    consulta = f"estudiante_aprobado('{nombre}', {puntos}, {asistencia}, {resultado})."
+nombre = input('Por favor, ingresa el nombre del estudiante: ')
 
-    respuesta = list(prolog.query(consulta))
+puntos = float(input('Ingresa la nota (de 0 a 5): '))
 
-    if respuesta:
-
-  
-
-        estado = respuesta[0][resultado]
-
-        print(f"{nombre} está {estado}.")
-
-    else:
-
-        print("No se pudo determinar el estado de aprobación.")
-
-
-
-nombre = input('Por favor, ingresa tu nombre: ')
-
-puntos = float(input('Ingresa tus puntos (de 0 a 5): '))
-
-asistencia = float(input('Ingresa tu porcentaje de asistencia (de 0 a 100): '))
+asistencia = float(input('Ingresa el porcentaje de asistencia (de 0 a 100): '))
 
 
 
