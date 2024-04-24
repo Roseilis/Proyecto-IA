675cd922049501b9d95c4a64fe7bb25c28efbcc3
roseilis <160541184+Roseilis@users.noreply.github.com>
2024-04-24 11:06:46 -0400

    Consulta.pl

--- Consulta.pl
+++ Consulta.pl
@@ -4,7 +4,7 @@ estudiante_aprobado(Nombre, Puntos, Asistencia) :-
     Asistencia >= 80,
     format('~w está aprobado.~n', [Nombre]).
 
-estudiante_aprobado(Nombre, Puntos, Asistencia) :-
+estudiante_desaprobado(Nombre, Puntos, Asistencia) :-
     Puntos < 3,
     Asistencia < 80,
     format('~w está desaprobado. Se sugiere mejorar la asistencia y obtener más puntos.~n', [Nombre]).
