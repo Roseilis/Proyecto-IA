835bd1bff2d1d0c1e917d6da794cff015af1b239
roseilis <160541184+Roseilis@users.noreply.github.com>
2024-04-24 11:00:20 -0400

    Create Consulta.pl

---
+++ Consulta.pl
@@ -0,0 +1,10 @@
+% Reglas de Prolog para determinar si un estudiante está aprobado
+estudiante_aprobado(Nombre, Puntos, Asistencia) :-
+    Puntos >= 3,
+    Asistencia >= 80,
+    format('~w está aprobado.~n', [Nombre]).
+
+estudiante_aprobado(Nombre, Puntos, Asistencia) :-
+    Puntos < 3,
+    Asistencia < 80,
+    format('~w está desaprobado. Se sugiere mejorar la asistencia y obtener más puntos.~n', [Nombre]).
